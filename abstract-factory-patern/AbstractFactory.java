e8e29869dca552b69d46b6ddb72e12e4fbcd98cd
Dominic Letarte <36549301+domincl@users.noreply.github.com>
2020-11-02 17:41:58 -0500

    Added png images and java examples

---
+++ abstract-factory-patern/AbstractFactory.java
@@ -0,0 +1,5 @@
+
+
+public abstract class AbstractFactory {
+   abstract Shape getShape(String shapeType) ;
+}
---
+++ abstract-factory-patern/RoundedSquare.java
@@ -0,0 +1,8 @@
+
+
+public class RoundedSquare implements Shape {
+   @Override
+   public void draw() {
+      System.out.println("Inside RoundedSquare::draw() method.");
+   }
+}
