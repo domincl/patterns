cf711bec9a720ec73d3d9f22bd78951082b2e3fc
Dominic Letarte <36549301+domincl@users.noreply.github.com>
2020-11-02 08:42:32 -0500

    Example code
    from https://www.tutorialspoint.com/design_pattern/factory_pattern.htm

---
+++ ShapeDemo.java
@@ -0,0 +1,25 @@
+
+
+public class ShapeDemo {
+
+   public static void main(String[] args) {
+
+      //get an object of Circle and call its draw method.
+      Shape shape1 = new Circle();
+
+      //call draw method of Circle
+      shape1.draw();
+
+      //get an object of Rectangle and call its draw method.
+      Shape shape2 = new Rectangle();
+
+      //call draw method of Rectangle
+      shape2.draw();
+
+      //get an object of Square and call its draw method.
+      Shape shape3 = new Square();
+
+      //call draw method of square
+      shape3.draw();
+   }
+}
